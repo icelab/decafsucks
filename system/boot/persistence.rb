f14b1a3723cacd352a4413506c914fa14d32f63d
Tim Riley <tim@openmonkey.com>
2018-10-24 09:46:17 +1100

    Use snowflakes-provided persistence component
    The component has been updated so we can pass auto-registration config to it, which means rom can now properly load from our DecafSucks::Persistence namespace

--- system/boot/persistence.rb
+++ system/boot/persistence.rb
@@ -1,37 +1,9 @@
-DecafSucks::Container.boot :persistence, namespace: true do |container|
-  init do
-    require "sequel"
-    require "rom"
-    require "rom/sql"
-
-    use :settings
-
-    ROM::SQL.load_extensions(:postgres)
-
-    Sequel.database_timezone = :utc
-    Sequel.application_timezone = :local
-
-    rom_config = ROM::Configuration.new(
-      :sql,
-      container[:settings].database_url,
-      extensions: %i[error_sql pg_array pg_json pg_enum],
-    )
-
-    rom_config.plugin :sql, relations: :instrumentation do |plugin_config|
-      plugin_config.notifications = notifications
-    end
-
-    rom_config.plugin :sql, relations: :auto_restrictions
-
-    register "config", rom_config
-    register "db", rom_config.gateways[:default].connection
-  end
-
-  start do
-    config = container["persistence.config"]
-
-    config.auto_registration container.root.join("lib/decaf_sucks/persistence"), namespace: "DecafSucks::Persistence"
-
-    register "rom", ROM.container(config)
+DecafSucks::Container.boot :persistence, namespace: true, from: :snowflakes do
+  configure do |config|
+    config.database_url = container.settings.database_url
+    config.global_extensions = [:postgres]
+    config.connection_extensions = %i[error_sql pg_array pg_json pg_enum]
+    config.auto_registration_root = container.root.join("lib/decaf_sucks/persistence")
+    config.auto_registration_namespace = "DecafSucks::Persistence"
   end
 end
