441950056268e93b558cf13d868f6dad5d2b0be8
Tim Riley <tim@openmonkey.com>
2019-04-28 10:05:54 +1000

    Remove per-slice bootable web deps
    These aren't needed anymore

--- slices/admin/system/boot/web.rb
+++
@@ -1,9 +0,0 @@
-DecafSucks::Admin::Slice.boot :web, namespace: true do |container|
-  init do
-    require "hanami/controller"
-  end
-
-  start do
-    register "action.configuration", Hanami::Controller::Configuration.new
-  end
-end
--- slices/main/system/boot/web.rb
+++
@@ -1,9 +0,0 @@
-DecafSucks::Main::Slice.boot :web, namespace: true do |container|
-  init do
-    require "hanami/controller"
-  end
-
-  start do
-    register "action.configuration", Hanami::Controller::Configuration.new
-  end
-end
